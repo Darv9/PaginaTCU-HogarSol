b9ad2a70d897449b8671d55c2c4ce95bb77236ef
Daniel RV <daniel.rave0903@gmail.com>
2024-10-28 18:00:14 -0600

    Se crea el footer y el header

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asociación Infantil Hogar Sol</title>
+    <link rel="stylesheet" href="../public/header-footer/css/headerStyle.css">
+    <link rel="stylesheet" href="../public/header-footer/css/footerStyle.css">
+    <script src="https://kit.fontawesome.com/186b29df68.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- Nos traemos el header prefab -->
+    <?php 
+        include("../public/header-footer/header.html"); 
+    ?>
+    <div class="content">
+        <h1>Hola</h1>
+    </div>
+    <?php 
+        include("../public/header-footer/footer.html"); 
+    ?>
+</body>
+</html>
